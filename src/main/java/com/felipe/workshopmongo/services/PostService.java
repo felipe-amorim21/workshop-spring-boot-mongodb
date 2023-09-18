ad8fdffe558ba7627ab5230d0d8d5ca498fdc522
Felipe Amorim <felipe21fab@gmail.com>
2023-09-18 19:46:37 -0300

    Obtendo um post por id

---
+++ src/main/java/com/felipe/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.felipe.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipe.workshopmongo.domain.Post;
+import com.felipe.workshopmongo.repository.PostRepository;
+import com.felipe.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
